da9c530fba889782b6926a648a783d143311446c
fummicc1 <fumiya.tennis1@gmail.com>
2021-12-28 10:31:43 +0900

    :bug: Work moya

--- Domain/Repository/HTTPClient.swift
+++ Domain/Repository/HTTPClient.swift
@@ -1,5 +1,50 @@
 import Foundation
+import Combine
+import Moya
 
+public protocol HTTPClient {
+    func send<Response: Codable>(_ service: MyService) async throws -> Response
+    func send(_ service: MyService) async throws
+}
 
 public struct HTTPClientImpl {
+    private let provider = MoyaProvider<MyService>()
+    private let decoder: JSONDecoder = JSONDecoder()
+}
+
+extension HTTPClientImpl: HTTPClient {
+    public func send(_ service: MyService) async throws {
+        let _: Void = try await withCheckedThrowingContinuation { continuation in
+            provider.request(service) { result in
+                switch result {
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+
+                case .success:
+                    continuation.resume(returning: ())
+                }
+            }
+        }
+    }
+
+    public func send<Response>(_ service: MyService) async throws -> Response where Response : Decodable, Response : Encodable {
+        let response: Response = try await withCheckedThrowingContinuation { continuation in
+            provider.request(service) { result in
+                switch result {
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+
+                case .success(let response):
+                    let data = response.data
+                    do {
+                        let model = try decoder.decode(Response.self, from: data)
+                        continuation.resume(returning: model)
+                    } catch {
+                        continuation.resume(throwing: error)
+                    }
+                }
+            }
+        }
+        return response
+    }
 }
